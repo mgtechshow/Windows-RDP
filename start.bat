c83a49c6f768819c0715bdc5ec12004091773085
MG TECH SHOW <113914824+mgtechshow@users.noreply.github.com>
2022-09-26 08:21:59 +0530

    Add files via upload

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By AvishkarPatil" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @mgtechshow /add >nul
+net user administrator @datpusheen123 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -16,8 +16,8 @@ echo  Successfully Installed !, If the RDP is Dead, Please Re Run Job Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get the NGROK tunnel, make sure the NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels"
 echo Username: administrator
-echo Password: @mgtechshow
+echo Password: @datpusheen123
 echo .
-echo  RDP By MG TECh SHOW
+echo  RDP By Push33n
 echo   Please Login to Your RDP !
 ping -n 10 127.0.0.1 >nul
